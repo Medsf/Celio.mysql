0de0bb2787af169aa3fbf4a83c2bfaa784717129
Medsf <eduardasfduda15@gmail.com>
2021-09-01 17:34:03 -0300

    Aula Celio

---
+++ pesquisarEstados.sql
@@ -0,0 +1,13 @@
+select * from estados;
+
+select nome as 'Nome estados',sigla,regiao as região from estados;
+
+select nome as 'Nome estados',sigla UF,regiao as região from estados;
+
+select * now() from estados where regiiao = 'sudeste';
+
+select * from estados where populacao > 10.00;
+
+select * from estados where populacao <= 10.00;
+
+select count(nome) as 'numero de estados centro-oeste' from estados where regiao = 'centro-oeste';
